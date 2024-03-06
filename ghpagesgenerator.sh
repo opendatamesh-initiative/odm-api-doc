18ce3f6e3264545b94e3818df1632790b5a8c0a9
Mattia Pennati <mattia.pennati@quantyca.it>
2024-03-06 18:36:01 +0100

    Minor fixes

--- ghpagesgenerator.sh
+++ ghpagesgenerator.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# Generate index.html for GitHub Pages
 echo "<html><body>" > index.html
 echo "<h2>ODM Api Documentation</h2>" >> index.html
-ls -R -1 | grep -vE 'index\.html|README\.md|ghpagesgenerator\.sh' | awk '!/\.$/ && /\.html$/{sub(/[^\/]+\//,"&<br/>")}1' >> index.html
+echo "<ul>" >> index.html
+# List versions and their respective links
+for dir in */; do
+  echo "<li>$dir" >> index.html
+  echo "<ul>" >> index.html
+  # List HTML files inside each version directory
+  for file in $dir*.html; do
+    if [ "$file" != "$dir/index.html" ]; then
+      echo "<li><a href=\"$file\">$(basename $file)</a></li>" >> index.html
+    fi
+  done
+  echo "</ul>" >> index.html
+  echo "</li>" >> index.html
+done
+echo "</ul>" >> index.html
 echo "</body></html>" >> index.html
