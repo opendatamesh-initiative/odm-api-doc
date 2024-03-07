9bcd6118649d3e47e5a2b7f60c7f5a8b2b56bb90
Mattia Pennati <mattia.pennati@quantyca.it>
2024-03-07 20:02:54 +0100

    minor fixes

--- ghpagesgenerator.sh
+++ ghpagesgenerator.sh
@@ -7,7 +7,7 @@ echo "<meta charset=\"UTF-8\">" >> doc.html
 echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" >> doc.html
 echo "<title>ODM API Documentation - Doc</title>" >> doc.html
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\">" >> doc.html
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">" >> doc.html
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style/styles.css\">" >> doc.html
 echo "<link rel=\"icon\" type=\"image/png\" href=\"images/favicon.ico\">" >> doc.html
 echo "<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">" >> doc.html
 echo "<style>" >> doc.html
@@ -22,7 +22,7 @@ echo "<h4>API Documentation</h4>" >> doc.html
 echo "<ul class=\"collection\">" >> doc.html
 # List version directories and their content
 for dir in */; do
-    if [[ "$dir" != "images/" ]]; then
+    if [[ "$dir" != "images/" && "$dir" != "javascript/" && "$dir" != "style/" ]]; then
         echo "<li onclick=\"toggleList(this)\" class=\"collection-item\">$dir" >> doc.html
         echo "<i class=\"material-icons arrow\">arrow_drop_down</i>" >> doc.html
         echo "<ul class=\"sublist\">" >> doc.html
@@ -38,34 +38,5 @@ for dir in */; do
 done
 echo "</ul>" >> doc.html
 echo "</div>" >> doc.html
-echo "<script>
-document.addEventListener('DOMContentLoaded', function() {
-  var navbarContainer = document.getElementById('navbar');
-  fetch('navbar.html')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to load navbar: ' + response.status);
-      }
-      return response.text();
-    })
-    .then(navbarHtml => {
-      navbarContainer.innerHTML = navbarHtml;
-      var activeLink = document.getElementById('doc-link');
-      activeLink.classList.add('active');
-    })
-    .catch(error => {
-      console.error(error);
-    });
-});
-function toggleList(element) {
-  var sublist = element.querySelector('.sublist');
-  sublist.classList.toggle('active');
-  var arrow = element.querySelector('.arrow');
-  if (sublist.classList.contains('active')) {
-    arrow.textContent = 'arrow_drop_up';
-  } else {
-    arrow.textContent = 'arrow_drop_down';
-  }
-}
-</script>" >> doc.html
+echo "<script src=\"javascript/doc.js\"></script>" >> doc.html
 echo "</body></html>" >> doc.html
